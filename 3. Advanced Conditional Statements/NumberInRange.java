93ad918d6730feed7711646eb03c200e8e3f3e9e
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-02-08 17:23:57 +0200

    Add exercise

---
+++ 3. Advanced Conditional Statements/NumberInRange.java
@@ -0,0 +1,22 @@
+package AdvancedConditionalStatements;
+
+import java.util.Scanner;
+
+public class NumberInRange
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if(number >= -100 && number <= 100 && number != 0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
