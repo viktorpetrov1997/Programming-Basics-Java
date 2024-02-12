05a30a617cbfb73b69d1a96fbb886739b17ab1f1
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-02-12 19:27:07 +0200

    Add exercise

---
+++ 3. Advanced Conditional Statements/CinemaTicket.java
@@ -0,0 +1,38 @@
+package AdvancedConditionalStatements;
+
+import java.util.Scanner;
+
+public class CinemaTicket
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        String day = scanner.nextLine();
+
+        switch(day)
+        {
+            case "Monday":
+                System.out.println(12);
+                break;
+            case "Tuesday":
+                System.out.println(12);
+                break;
+            case "Wednesday":
+                System.out.println(14);
+                break;
+            case "Thursday":
+                System.out.println(14);
+                break;
+            case "Friday":
+                System.out.println(12);
+                break;
+            case "Saturday":
+                System.out.println(16);
+                break;
+            case "Sunday":
+                System.out.println(16);
+                break;
+        }
+    }
+}
