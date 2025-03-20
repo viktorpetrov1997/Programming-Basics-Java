f5c1899b669998e7fe72feb62e1bc7c9d4124bfd
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2025-03-20 23:27:45 +0200

    Add exercise

---
+++ 4. For Loop/Lab/NumbersFrom1to100.java
@@ -0,0 +1,16 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class NumbersFrom1to100
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 1; i <= 100; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
