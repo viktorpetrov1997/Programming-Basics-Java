46ed0ac6d886983771e4dc8e0b2d05f08f175c7f
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2025-03-27 13:11:20 +0200

    Add exercise

---
+++ 4. For Loop/Lab/CharacterSequence.java
@@ -0,0 +1,19 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class CharacterSequence
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            char letter = input.charAt(i);
+            System.out.println(letter);
+        }
+    }
+}
