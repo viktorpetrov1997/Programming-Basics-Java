3b3e65f21503f733b4b6847738d1e4e1ec6fb4ce
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2025-04-07 16:49:48 +0300

    Add folder and exercise

---
+++ 1. First Steps In Coding/MoreExercises/TrapezoidArea.java
@@ -0,0 +1,19 @@
+package FirstStepsInCoding.MoreExercises;
+
+import java.util.Scanner;
+
+public class TrapezoidArea
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        double b1 = Double.parseDouble(scanner.nextLine());
+        double b2 = Double.parseDouble(scanner.nextLine());
+        double h = Double.parseDouble(scanner.nextLine());
+
+        double area = (b1 + b2) * h / 2;
+
+        System.out.printf("%.2f", area);
+    }
+}
